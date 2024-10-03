ad46fea6c4ee483d02c17b90aebf9d52aa606250
vanyscore <vanyscore@gmail.com>
2024-10-03 18:20:37 +0500

    Notes page padding fix.

--- app/src/main/java/com/vanyscore/notes/NotesPage.kt
+++ app/src/main/java/com/vanyscore/notes/NotesPage.kt
@@ -1,9 +1,11 @@
 package com.vanyscore.notes
 
+import android.annotation.SuppressLint
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -15,6 +17,7 @@ import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
 import com.vanyscore.notes.viewmodel.NotesViewModel
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 @Composable
 fun NotesPage(
     viewModel: NotesViewModel = viewModel()
@@ -22,22 +25,19 @@ fun NotesPage(
     val state = viewModel.state.collectAsState().value
     val notes = state.notes
     return Scaffold {
-        Box(modifier = Modifier
-            .fillMaxSize()
-            .padding(it),
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxSize(),
+            contentPadding = PaddingValues(horizontal = 16.dp, vertical = 16.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
         ) {
-            LazyColumn(
-                contentPadding = PaddingValues(horizontal = 16.dp),
-                verticalArrangement = Arrangement.spacedBy(16.dp),
-            ) {
-                items(
-                    notes
-                ) { note ->
-                    NoteItem(
-                        note = note,
-                        onClick = {}
-                    )
-                }
+            items(
+                notes
+            ) { note ->
+                NoteItem(
+                    note = note,
+                    onClick = {}
+                )
             }
         }
     }
