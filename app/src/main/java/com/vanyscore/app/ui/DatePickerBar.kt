bbe4882a52a19c4186b06ab6308f747f602b7734
vanyscore <vanyscore@gmail.com>
2025-06-24 18:54:29 +0500

    Added settings button for top app bar.

--- app/src/main/java/com/vanyscore/app/ui/DatePickerBar.kt
+++ app/src/main/java/com/vanyscore/app/ui/DatePickerBar.kt
@@ -11,6 +11,7 @@ import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.KeyboardArrowLeft
 import androidx.compose.material.icons.filled.KeyboardArrowRight
+import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.DatePicker
 import androidx.compose.material3.DatePickerDialog
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -22,11 +23,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults.topAppBarColors
 import androidx.compose.material3.rememberDatePickerState
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.unit.dp
@@ -39,25 +42,17 @@ import java.util.Locale
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun DatePickerBar() {
-    val yearFormat = remember {
-        SimpleDateFormat("yyyy", Locale.getDefault())
-    }
-    val monthFormat = remember {
-        SimpleDateFormat("MMMM", Locale.getDefault())
-    }
-    val dayFormat = remember {
-        SimpleDateFormat("dd", Locale.getDefault())
-    }
-    val state = AppState.source.collectAsState()
-    val currentDate = state.value.date
-    val textStyle = TextStyle(fontSize = 16.sp, color = Color.White)
     val dialogState = remember {
         mutableStateOf(false)
     }
-    val appState = state.value
     return TopAppBar(
         modifier = Modifier
             .height(56.dp),
+        navigationIcon = {
+            SettingsButton(
+                isVisible = false
+            )
+        },
         title = {
             Box(
                 modifier = Modifier
@@ -71,70 +66,129 @@ fun DatePickerBar() {
                 Row(
                     verticalAlignment = Alignment.CenterVertically
                 ) {
-                    IconButton(
-                        onClick = {
-                            AppState.updateState(appState.copy(
-                                date = Calendar.getInstance().apply {
-                                    time = currentDate
-                                    add(Calendar.DAY_OF_MONTH, -1)
-                                }.time
-                            ))
-                        },
-                    ) {
-                        Icon(
-                            Icons.Default.KeyboardArrowLeft, "Left", tint = Color.White
-                        )
-                    }
-                    Text(dayFormat.format(currentDate), style = textStyle)
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(monthFormat.format(currentDate), style = textStyle)
-                    Spacer(modifier = Modifier.width(8.dp))
-                    Text(yearFormat.format(currentDate), style = textStyle)
-                    IconButton(
-                        onClick = {
-                            AppState.updateState(appState.copy(
-                                date = Calendar.getInstance().apply {
-                                    time = currentDate
-                                    add(Calendar.DAY_OF_MONTH, 1)
-                                }.time
-                            ))
-                        }
-                    ) {
-                        Icon(
-                            Icons.Default.KeyboardArrowRight, "Right", tint = Color.White
-                        )
-                    }
+                    DateControl()
                 }
             }
             if (dialogState.value) {
-                val datePickerState = rememberDatePickerState(
-                    initialDisplayedMonthMillis = state.value.date.time,
-                )
-                DatePickerDialog(
-                    onDismissRequest = {
-                        dialogState.value = false
-                    },
-                    confirmButton = {
-                        val selectedDateInMillis = datePickerState.selectedDateMillis
-                        if (selectedDateInMillis != null) {
-                            dialogState.value = false
-                            AppState.updateState(appState.copy(
-                                date = Calendar.getInstance().apply {
-                                    timeInMillis = selectedDateInMillis
-                                }.time
-                            ))
-                        }
-                    },
-                ) {
-                    val currDateInMillis = Calendar.getInstance().timeInMillis
-                    DatePicker(state = datePickerState, dateValidator = { time ->
-                        time <= currDateInMillis
-                    })
-                }
+                DatePickerDialogBuild(dialogState)
             }
+        },
+        actions = {
+            SettingsButton()
         },
         colors = topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primary
         )
     )
+}
+
+@Composable
+fun SettingsButton(isVisible: Boolean = true) {
+    IconButton(
+        modifier = Modifier.alpha(if (isVisible) 1f else 0f),
+        onClick = {
+            // TODO(vanyscore): Make theme pick dialog.
+        }
+    ) {
+        Icon(
+            imageVector = Icons.Default.Settings,
+            contentDescription = "Settings" // Accessibility label
+        )
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun DatePickerDialogBuild(dialogState: MutableState<Boolean>) {
+    val state = AppState.source.collectAsState()
+    val appState = state.value
+    val datePickerState = rememberDatePickerState(
+        initialDisplayedMonthMillis = state.value.date.time,
+    )
+    DatePickerDialog(
+        onDismissRequest = {
+            dialogState.value = false
+        },
+        confirmButton = {
+            val selectedDateInMillis = datePickerState.selectedDateMillis
+            if (selectedDateInMillis != null) {
+                dialogState.value = false
+                AppState.updateState(appState.copy(
+                    date = Calendar.getInstance().apply {
+                        timeInMillis = selectedDateInMillis
+                    }.time
+                ))
+            }
+        },
+    ) {
+        val currDateInMillis = Calendar.getInstance().timeInMillis
+        DatePicker(state = datePickerState, dateValidator = { time ->
+            time <= currDateInMillis
+        })
+    }
+}
+
+@Composable
+fun DateControl() {
+    val state = AppState.source.collectAsState()
+    val currentDate = state.value.date
+    val textStyle = TextStyle(fontSize = 16.sp, color = Color.White)
+    val yearFormat = remember {
+        SimpleDateFormat("yyyy", Locale.getDefault())
+    }
+    val monthFormat = remember {
+        SimpleDateFormat("MMMM", Locale.getDefault())
+    }
+    val dayFormat = remember {
+        SimpleDateFormat("dd", Locale.getDefault())
+    }
+    PrevDateButton()
+    Text(dayFormat.format(currentDate), style = textStyle)
+    Spacer(modifier = Modifier.width(8.dp))
+    Text(monthFormat.format(currentDate), style = textStyle)
+    Spacer(modifier = Modifier.width(8.dp))
+    Text(yearFormat.format(currentDate), style = textStyle)
+    NextDateButton()
+}
+
+@Composable
+private fun PrevDateButton() {
+    val appState = AppState.source.collectAsState()
+    val state = appState.value
+    val currentDate = appState.value.date
+    IconButton(
+        onClick = {
+            AppState.updateState(state.copy(
+                date = Calendar.getInstance().apply {
+                    time = currentDate
+                    add(Calendar.DAY_OF_MONTH, -1)
+                }.time
+            ))
+        },
+    ) {
+        Icon(
+            Icons.Default.KeyboardArrowLeft, "Left", tint = Color.White
+        )
+    }
+}
+
+@Composable
+private fun NextDateButton() {
+    val appState = AppState.source.collectAsState()
+    val state = appState.value
+    val currentDate = appState.value.date
+    IconButton(
+        onClick = {
+            AppState.updateState(state.copy(
+                date = Calendar.getInstance().apply {
+                    time = currentDate
+                    add(Calendar.DAY_OF_MONTH, 1)
+                }.time
+            ))
+        }
+    ) {
+        Icon(
+            Icons.Default.KeyboardArrowRight, "Right", tint = Color.White
+        )
+    }
 }
